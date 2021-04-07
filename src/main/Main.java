1864937c98d719cde38853abf62ed85e5848abb1
Nizar <nizar4790k@gmail.com>
2021-04-07 13:40:53 -0400

    primer ejercicio finalizado

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Bicicleta miBicicleta = new Bicicleta(2,1,1);
+        Bicicleta tuBicicleta = new Bicicleta(3,1,2);
     }
 
 }
