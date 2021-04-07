eff1df039fe11e8cacee40d4fb4fbbe4beedb868
Nizar <nizar4790k@gmail.com>
2021-04-07 13:38:17 -0400

    primer ejercicio finalizado

---
+++ src/main/Bicicleta.java
@@ -0,0 +1,36 @@
+package main;
+
+public class Bicicleta
+{
+
+    private int velocidadActual;
+    private int platoActual;
+    private int pinonActual;
+
+    public Bicicleta(int velocidadActual, int platoActual, int pinonActual) {
+        this.velocidadActual = velocidadActual;
+        this.platoActual = platoActual;
+        this.pinonActual = pinonActual;
+    }
+
+    public void acelerar()
+    {
+
+        velocidadActual *= 2;
+    }
+
+    public void frenar()
+    {
+        velocidadActual  *= 0.5;
+    }
+
+    public void cambiarPlato(int plato){
+        this.platoActual = plato;
+    }
+
+    public void cambiarPinon(int pinon){
+        this.pinonActual = pinon;
+    }
+
+
+}
